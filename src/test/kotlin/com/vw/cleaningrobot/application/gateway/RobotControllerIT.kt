fcd90dbe429ba2d91dbb4a418ab0094435d94603
victor <vibanez18@gmail.com>
2023-06-18 14:45:37 +0200

    add integration test

--- src/test/kotlin/com/vw/cleaningrobot/application/gateway/RobotControllerIT.kt
+++ src/test/kotlin/com/vw/cleaningrobot/application/gateway/RobotControllerIT.kt
@@ -1,13 +1,41 @@
 package com.vw.cleaningrobot.application.gateway
 
+import com.vw.cleaningrobot.domain.service.RobotDomainService
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@WebMvcTest
+@ComponentScan("com.vw.cleaningrobot")
 class RobotControllerIT {
 
+	@Autowired
+	lateinit var mockMvc: MockMvc
+
+	@Autowired
+	var robotDomainService: RobotDomainService = RobotDomainService()
+
 	@Test
-	fun contextLoads() {
+	fun `when call start with happy path returns 201`() {
+		mockMvc.perform(MockMvcRequestBuilders.post("/cleaning/start")
+			.contentType(MediaType.TEXT_PLAIN_VALUE)
+			.content(generateHappyPathDataInput())
+		).andExpect(status().isCreated)
 	}
 
+	private fun generateHappyPathDataInput() = """
+        5 5
+        1 2 N
+        LMLMLMLMM
+        3 3 E
+        MMRMMRMRRM
+    """.trimIndent()
 }
